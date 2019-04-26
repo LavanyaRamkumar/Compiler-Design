14d91c762591b58eafa1df70761d21e27e74be44
LavanyaRamkumar <lavanya.ramkumar99@gmail.com>
2019-04-26 11:49:53 +0530

    final commit

--- ast/ex.sh
+++
@@ -1,6 +0,0 @@
-rm y.tab.c
-rm y.tab.h
-yacc -d gram.y
-lex grammar1.l
-gcc y.tab.c lex.yy.c -ll -ly
-./a.out < inputs/ip1.c 
---
+++ readme.md
@@ -0,0 +1,6 @@
+SETUP:
+
+bash ex.sh INPUTFILENAME
+
+input file must be in the IO directory
+
